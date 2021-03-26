7e7ecd3f2bb5aa9b120209f00cd965691c3b311a
Hanno Rein <hanno@hanno-rein.de>
2021-03-26 18:11:45 +0100

    plot

--- check.py
+++ check.py
@@ -2,6 +2,16 @@ import tweepy
 import csv
 import datetime
 import requests
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import matplotlib.ticker as ticker
+
+
+
+def forward(x):
+    return x/pop*100.
+
+
 
 
 response = requests.get('https://raw.githubusercontent.com/ccodwg/Covid19Canada/master/timeseries_prov/vaccine_administration_timeseries_prov.csv', stream=True)
@@ -40,9 +50,34 @@ if recent[0].strftime("%Y-%m-%d") != lasttweet:
     print(text)
     with open("lasttweet.txt","w") as f:
         f.write(recent[0].strftime("%Y-%m-%d"))
-    api.update_status(text)
+    fig, ax = plt.subplots(1,1,figsize=(6,4))
+    ax.xaxis.set_major_locator(mdates.DayLocator())
+    ax.xaxis.set_major_formatter(mdates.DateFormatter('%-d/%-m'))
+
+    x = [r[0] for r in ontario[-7:]]
+    y = [r[1] for r in ontario[-7:]]
+    ax.set_xlim([x[0]-datetime.timedelta(days=0.6),x[-1]+datetime.timedelta(days=0.6)])
+    ax.axhline(sum(y)/7.,color="black",ls="--")
+    ax.annotate('7 day average: %.0f doses/day'%(sum(y)/7.), xy=(x[0]-datetime.timedelta(days=0.2), 1.03*sum(y)/7.))
+    ax.set_ylabel("Administered doses")
+    rects = ax.bar(x,y,color=(1,0.4,0.4))
+    for rect in rects:
+        height = rect.get_height()
+        ax.annotate('{} doses'.format(height),
+                    xy=(rect.get_x() + rect.get_width() / 2, 1000),
+                    xytext=(0, 3), 
+                    textcoords="offset points",
+                    ha='center', va='bottom', rotation=90)
+
+    rax = ax.twinx()
+    rax.set_yticks([forward(x) for x in ax.get_yticks()])
+    rax.set_ybound([forward(x) for x in ax.get_ybound()])
+    rax.yaxis.set_major_formatter(ticker.FormatStrFormatter('%.1f%%'))
+    rax.set_ylabel("Fraction of population")
+
+    fig.savefig("7day.png", transparent=False)
+
+    api.update_with_media("7day.png", text)
+
+    #api.update_status(text)
     
-
-
-
-
